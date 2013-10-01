0d42f525c61d82da72032218f9abbf6992cd991f
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-10-02 02:40:45 +0800

    Sorted the uri into modules

--- src/uri.ml
+++ src/uri.ml
@@ -1,29 +1,45 @@
 open Eliom_content.Html5.D
 
-let jquery_js_v2 =
-  js_script
-    (uri_of_string
-       (function () ->
-                 "//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"))
-    ()
+module JQuery = struct
+  module Min = struct
+    let v2 =
+      js_script
+        (uri_of_string
+           (function () ->
+                     "//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"))
+        ()
+  end
+end
 
-let css_v3 =
-  css_link
-    (uri_of_string
-       (function () ->
-                 "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"))
-    ()
+module Css = struct
+  module Min = struct
+    let v3 =
+      css_link
+        (uri_of_string
+           (function () ->
+                     "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"))
+        ()
+  end
+end
 
-let css_theme_v3 =
-  css_link
-    (uri_of_string
-       (function () ->
-                   "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css"))
-    ()
+module Theme = struct
+  module Min = struct
+    let v3 =
+      css_link
+        (uri_of_string
+           (function () ->
+                     "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css"))
+        ()
+  end
+end
 
-let js_v3 =
-  js_script
-    (uri_of_string
-       (function () ->
-                 "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"))
-    ()
+module Js = struct
+  module Min = struct
+    let v3 =
+      js_script
+        (uri_of_string
+           (function () ->
+                     "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"))
+        ()
+  end
+end
