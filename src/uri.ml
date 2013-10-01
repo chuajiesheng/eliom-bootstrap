0a98a890b72978a3566655445713d83473ea121a
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-10-02 03:00:26 +0800

    Generalize css and js link function

--- src/uri.ml
+++ src/uri.ml
@@ -1,45 +1,35 @@
 open Eliom_content.Html5.D
 
+let make_css url =
+  css_link (uri_of_string (function () -> url)) ()
+
+let make_js url =
+  js_script (uri_of_string (function () -> url)) ()
+
 module JQuery = struct
   module Min = struct
     let v2 =
-      js_script
-        (uri_of_string
-           (function () ->
-                     "//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"))
-        ()
+      make_js "//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"
   end
 end
 
 module Css = struct
   module Min = struct
     let v3 =
-      css_link
-        (uri_of_string
-           (function () ->
-                     "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"))
-        ()
+      make_css "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"
   end
 end
 
 module Theme = struct
   module Min = struct
     let v3 =
-      css_link
-        (uri_of_string
-           (function () ->
-                     "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css"))
-        ()
+      make_css "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css"
   end
 end
 
 module Js = struct
   module Min = struct
     let v3 =
-      js_script
-        (uri_of_string
-           (function () ->
-                     "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"))
-        ()
+      make_js "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"
   end
 end
